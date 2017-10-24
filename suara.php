2c64d5d6ac3e2d1871f5272628daf753937f2ca5
161710089 <161710089@smkassalaambandung.sch.id>
2017-10-24 17:22:10 +0700

    first commit

---
+++ suara.php
@@ -0,0 +1,20 @@
+<?php
+class macamsuara {
+public $suaraayam;
+public $suararobot;
+public $suaraburung;
+public $suaratikus;
+
+public function __construct($suaraayam,$suararobot,$suaraburung,$suaratikus)
+{
+$this->suaraayam =$suaraayam;
+$this->suararobot =$suararobot;
+$this->suaraburung =$suaraburung;
+$this->suaratikus =$suaratikus;
+}
+
+
+
+
+}
+  ?>
---
+++ suara1.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'suara.php';
+$suara =new macamsuara ('kukuruyuk','ngik ngik','perkututututut','cit cit cit');
+echo "suaraayam.. ".$suara->suaraayam."<br>";
+echo "suararobot.. ".$suara->suararobot."<br>";
+echo "suaraburung.. ".$suara->suaraburung."<br>";
+echo "suaratikus.. ".$suara->suaratikus."<br>";
+
+  ?>
